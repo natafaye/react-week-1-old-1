1157909b3c377f898d25e8641eec996a1ef36a42
Natalie Childs <nata.faye@gmail.com>
2021-06-17 21:52:54 -0700

    Added Farmhouse to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Barn from './components/Barn';
 import Garden from './components/garden';
+import Farmhouse from './components/Farmhouse';
 
 export default class App extends Component {
 	render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
 			<div className='container mt-3'>
 				<Barn />
 				<Garden />
+        		<Farmhouse />
 			</div>
 		);
 	}
